1ca7bc050943e17585306a5b721843f3ebe2a74b
martires alberto orozco gonzalez <trans_maog@htomail.com>
2019-12-30 19:08:04 -0600

    Actualizacion 29/12/2019 Desactivar notificaciones de laravelmix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,4 +11,4 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js');
+mix.js("resources/js/app.js", "public/js").disableSuccessNotifications();
