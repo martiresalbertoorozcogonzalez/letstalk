547a48a093617057bfb8cfdac295d826678eaea2
martires alberto orozco gonzalez <trans_maog@htomail.com>
2019-12-30 23:10:36 -0600

    Actualizacion 29/12/2019 Mas acerca de los Queue Jobs

--- app/observers/MessageObserver.php
+++ app/observers/MessageObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Message;
 use App\Conversation;
+use App\Events\MessageSent;
 
 class MessageObserver
 {
@@ -26,5 +27,7 @@ class MessageObserver
             $conversation->last_time = $message->created_at;
             $conversation->save();
         }
+
+        event(new MessageSent($message));
     }
 }
