52685c2b58af0ccbc6e10fcd9ec966e4026140d8
martires alberto orozco gonzalez <trans_maog@htomail.com>
2020-01-01 15:55:03 -0600

    Actualizacion 1/1/2020 Moment.js (Fechas con formato)

--- resources/js/components/ContactComponent.vue
+++ resources/js/components/ContactComponent.vue
@@ -1,39 +1,48 @@
 <template>
-  <b-list-group-item :variant="variant">
-    <b-row class="p-2" align-h="center">
-      <b-col cols="12" md="3" class="text-center">
-        <b-img
-          rounded="circle"
-          blank
-          width="60"
-          heigth="60"
-          blank-color="#777"
-          alt="Circle image"
-          class="m-1"
-        />
-      </b-col>
+    <b-list-group-item :variant="variant">
+        <b-row class="p-2" align-h="center">
+            <b-col cols="12" md="3" class="text-center">
+                <b-img
+                    rounded="circle"
+                    blank
+                    width="60"
+                    heigth="60"
+                    blank-color="#777"
+                    alt="Circle image"
+                    class="m-1"
+                />
+            </b-col>
 
-      <b-col cols="6" align-self="center" class="d-none d-md-block">
-        <p class="mb-1">{{ conversation.contact_name }}</p>
-        <p class="text-muted small mb-1">{{ conversation.last_message }}</p>
-      </b-col>
+            <b-col cols="6" align-self="center" class="d-none d-md-block">
+                <p class="mb-1">{{ conversation.contact_name }}</p>
+                <p class="text-muted small mb-1">
+                    {{ conversation.last_message }}
+                </p>
+            </b-col>
 
-      <b-col cols="3" class="d-none d-md-block">
-        <p class="text-muted small">{{ conversation.last_time }}</p>
-      </b-col>
-    </b-row>
-  </b-list-group-item>
+            <b-col cols="3" class="d-none d-md-block">
+                <p class="text-muted small">{{ lastTime }}</p>
+            </b-col>
+        </b-row>
+    </b-list-group-item>
 </template>
 
 <script>
 export default {
-  props: {
-    variant: String,
-    conversation: Object
-  },
-  data() {
-    return {};
-  },
-  mounted() {}
+    props: {
+        variant: String,
+        conversation: Object
+    },
+    data() {
+        return {};
+    },
+    mounted() {},
+    computed: {
+        lastTime() {
+            return moment(this.conversation.last_time, "YYYY-MM-DD hh:mm:ss")
+                .locale("es")
+                .fromNow();
+        }
+    }
 };
 </script>
